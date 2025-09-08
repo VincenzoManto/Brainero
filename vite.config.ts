243c1942805306e5ddbbf5400acb0718ac570531
VincenzoManto <vincmanto@gmail.com>
2025-09-08 12:42:26 +0200

    Set base path in vite.config.ts for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",
+  base: "/Brainero/", // Replace <repository-name> with your GitHub repository name
   server: {
     host: "::",
     port: 8080,
@@ -14,5 +14,8 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist", // Default output directory
   },
 }));
